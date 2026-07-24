283a9fa96a14ac1161374e538f703e6b13d788e9
Katie zhu <katiezhuxy06@gmail.com>
2026-07-24 15:12:55 +0800

    feat: yahoo api calculation

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from data.watchlist import get_watchlist
-from data.market_data import get_stock_info
+from data.market_data import get_stock_info, get_price_history
 from analysis.fundamental_analysis import calculate_fundamental_score
+from analysis.technical_analysis import calculate_technical_score
 
 
 watchlist = get_watchlist()
@@ -10,8 +11,20 @@ for stock in watchlist:
 
     stock_info = get_stock_info(stock)
 
-    result = calculate_fundamental_score(stock_info)
+    price_history = get_price_history(stock)
+
+
+    fundamental_score = calculate_fundamental_score(stock_info)
+
+    technical_score = calculate_technical_score(price_history)
+
 
     print(stock)
-    print(result)
+
+    print("Fundamental:")
+    print(fundamental_score)
+
+    print("Technical:")
+    print(technical_score)
+
     print("--------------------------------")
